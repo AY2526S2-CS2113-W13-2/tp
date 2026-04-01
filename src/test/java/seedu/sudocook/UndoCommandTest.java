7cb6e4f5fcdebcde018dae2c2fe7b6440f44294f
aidan17715 <aidan.chia.tong@gmail.com>
2026-04-02 00:28:26 +0800

    fix: remove unused import from UndoCommandTest
    - Removed unused static import of assertEquals
    - Fixes checkstyle UnusedImports violation

--- src/test/java/seedu/sudocook/UndoCommandTest.java
+++ src/test/java/seedu/sudocook/UndoCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.sudocook;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
